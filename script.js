3f1da823c8f7c5ef60f6069fa1ca0bf65ee1dc6c
Lucas <luks_soares94@hotmail.com>
2024-05-10 15:25:15 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -19,27 +19,74 @@ const barbearia = {
   
   
 function buscaCortePorId(id) {
+    for (let i = 0; i < barbearia.cortes.length; i++){
+        if (id === barbearia.cortes[i].id){
+            return barbearia.cortes[i]
+        }
+    }
+    return "Corte não encontrado"
 }
+buscaCortePorId(1)
 
 function buscaBarbaPorId(id) {
+    for (let i = 0; i < barbearia.barbas.length; i++){
+        if (id === barbearia.barbas[i].id){
+            return barbearia.barbas[i]
+        }
+    }
+    return "Barba não encontrada"
 }
+buscaBarbaPorId(1)
 
 function verificaStatusBarbearia() {
+    if (barbearia.estaAberto){
+        return "Estamos abertos"
+    }
+    return "Estamos fechados"
 }
+verificaStatusBarbearia()
 
 function retornaTodosCortes() {
+    return barbearia.cortes
 }
+retornaTodosCortes()
 
 function retornaTodasBarbas() {
+    return barbearia.barbas
 }
+retornaTodasBarbas()
 
 function criaPedido(nomeCliente, corteId, barbaId) {
+    const corte = buscaCortePorId(corteId)
+    const barba = buscaBarbaPorId(barbaId)
+
+    const pedido = {
+        nome: nomeCliente,
+        pedidoCorte: corte.tipo,
+        pedidoCortePreco: corte.valor,
+        pedidoBarba: barba.tipo,
+        pedidoBarbaPreco: barba.valor
+    }
+    return pedido
 }
+criaPedido("Lucas", 1, 2)
+
+const novoCorte = {id: 1, tipo: "Highfade", valor: 25}
 
 function atualizarServico(lista, id, valor, tipo) {
+    for (let i = 0; i < lista.length; i++){
+        if (id === lista[i].id){
+            lista[i].tipo = tipo
+            lista[i].valor = valor
+            break
+        }
+    }
+    return lista   
 }
+atualizarServico(barbearia, 2, 20, "High fade")
 
 function calculaTotal(pedido) {
+    let valorTotal = pedido.pedidoCortePreco + pedido.pedidoBarbaPreco
+    return valorTotal
 }
-  
-  
+calculaTotal(criaPedido("Lucas", 1, 2))
